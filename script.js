6809847ee9c81f4bc211314d6b30c6f29cc06360
elpe2005 <Epedersen2005@gmail.com>
2023-08-30 09:19:10 +0200

    Uppgift 1 klar

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const num1 = prompt("nummer 1");
+/* const num1 = prompt("nummer 1");
 const num2 = prompt("nummer 2");
 const sum = Number(num1) + Number(num2);
 
 if (sum === 72) {
 	console.log(`Du är en delfin som är ${sum} år`); // "Du är en delfin som är ", sum, " år"
 } else {
+	document.getElementById("svar").innerHTML = "New text!";
 	console.log(`${sum}`);
 }
+ */
