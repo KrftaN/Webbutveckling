8e51d07eb228ffb39a1cda4fb88b48653c105c67
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-08 11:27:19 +0200

    Grid layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* const num1 = prompt("nummer 1");
+const num1 = prompt("nummer 2");
 const num2 = prompt("nummer 2");
 const sum = Number(num1) + Number(num2);
 
@@ -8,4 +8,3 @@ if (sum === 72) {
 	document.getElementById("svar").innerHTML = "New text!";
 	console.log(`${sum}`);
 }
- */
